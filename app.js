201f15edd1b5d0459fc2b7515abe402a326bfa8d
Aaron Wilson <aaronwilson@Tests-MacBook-Pro.local>
2020-12-03 20:20:57 -0700

    user can type in city and have a list of breweries returned

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ May need different buttons depending on how the user wants to search ex. state, 
 
 get city button and box working and then go from there
 
+make a for loop that cycles through the length of the array and displays results with matching city
+
+trying to make a variable that is equal to a string of the input box value and put it into my url using string interpolation
+
+try using jQuery to make a div on input click with the text
+
+variable worked once I put the AJAX inside of a on click function. without doing that the AJAX would load as soon as the page was refreshed
+
+
 could possibly make 5 buttons, state,city,zip,ect. with 5 hidden input boxes stacked on top of eachother and make on click event to display input box to corrisponding button
 
 layout HTML (form w/ buttons and optional radius)
@@ -21,8 +30,24 @@ basic CSS styling to make data legible
 ==============================================================
 GRAVEYARD
 ==============================================================
+for(let i = 0; i < data.length; i++) {
 
+}//end for loop
 
+//console.log($userInput);
+//console.log(link);
+
+// const city = () => {
+//
+// //let $userInput = $(`input[type="text"]`).val()
+// console.log($userInput);
+// return $userInput
+// console.log(data);
+//
+// }//end of city function
+  // $(`button`).on(`click`, city)
+
+      //url: `https://api.openbrewerydb.org/breweries?by_city=murrieta`
 
 
 
@@ -35,25 +60,53 @@ MEAT N' POTATOES
 // console.log($);
 
 
+
+
+
 $(() => {
   //jQuery code goes here
 
-  $.ajax({
+  $(`.button`).on(`click`,() => {
 
-    url: `https://api.openbrewerydb.org/breweries?by_city=murrieta`
+    let $userInput = $('input[type="text"]').val()
+
+    let link = `https://api.openbrewerydb.org/breweries?by_city=${$userInput}`
+
+    $.ajax({
+
+      url: link
+
+    })//end of ajax
+      .then(
+        (data) => {
+          console.log(data);
 
 
-  })//end of ajax
-    .then(
-      (data) => {
-        console.log(data);
+        const getInfo = () => {
 
-      },//end of success function(data)
-      (error) => {
-        console.log(`error`);
-      }//end of error function(error)
-    )//end of .then()
+          for(let i = 0; i < data.length; i++) {
 
+            let $name = $(`<li>`).text(`${data[i].name}`).appendTo(`.name`)
+            let $city = $(`<li>`).text(`${data[i].city}`).appendTo(`.city`)
+            let $street = $(`<li>`).text(`${data[i].street}`).appendTo(`.street`)
+            let $state = $(`<li>`).text(`${data[i].state}`).appendTo(`.state`)
+            let $postal = $(`<li>`).text(`${data[i].postal_code}`).appendTo(`.postal`)
+
+
+
+          }//end for loop
+
+        }//end getName
+
+        getInfo()
+
+        },//end of success function(data)
+        (error) => {
+          console.log(`error`);
+        }//end of error function(error)
+      )//end of .then()
+
+  })//end of on click event for submit button
 
 
 });//end of jQuery
